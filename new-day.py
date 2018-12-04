f5b8af70864a154a310bb2bcedd3968c7813430d
Alex Gulliver <randdalf@gmail.com>
2018-12-04 20:49:46 +0000

    Add utility that creates script skeletons for the current day

---
+++ new-day.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env/python
+
+"""Generate a skeleton program for Advent of Code"""
+
+import datetime
+import pathlib
+
+solution_template='''#!/usr/bin/env python
+
+"""Advent of Code 2018, Day {day}"""
+
+from aoc18 import solve
+
+def parse(data):
+    pass
+
+if __name__ == "__main__":
+    solve({day}, parse)'''
+
+test_template='''
+#!/usr/bin/env python
+
+"""Advent of Code 2018, Day {day} (Unit Tests)"""
+
+import unittest
+
+class SolverTests(unittest.TestCase):
+    pass
+
+if __name__ == "__main__":
+    unittest.main()
+'''
+
+def write_skeleton(day, template, filename_format):
+    skeleton = template.format(day=day)
+    filename = filename_format.format(day=day)
+    if not pathlib.Path(filename).exists():
+        with open(filename, 'w+') as file:
+            file.write(skeleton)
+            print('Created {filename}'.format(filename=filename))
+    else:
+        print('{filename} already exists'.format(filename=filename))
+
+def main():
+    day = datetime.datetime.now().day
+    write_skeleton(day, solution_template, 'd{day:02d}.py')
+    write_skeleton(day, test_template, 'd{day:02d}-test.py')
+
+if __name__ == "__main__":
+    main()
