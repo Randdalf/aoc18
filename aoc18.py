cd6bb2684cd747d020f906efbf41cc707bc71aa9
Alex Gulliver <randdalf@gmail.com>
2018-12-05 08:24:40 +0000

    Report the performance of the solvers

--- aoc18.py
+++ aoc18.py
@@ -3,9 +3,13 @@
 """Advent of Code 2018"""
 
 import aocd
+from time import perf_counter
 
 def run_solver(day, level, solver, input):
-    print(solver(input))
+    start = perf_counter()
+    answer = solver(input)
+    end = perf_counter()
+    print('{0:<30} ({1:.4f}s)'.format(answer, end - start))
 
 def solve(day, parse, *solvers):
     data = aocd.get_data(day=day, year=2018)
