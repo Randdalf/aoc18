d4f5625a80e6feee343cd700ba8485f88cadc616
Alex Gulliver <randdalf@gmail.com>
2018-12-11 10:06:18 +0000

    Update Day 11, Puzzle 2 to use a summed area table

--- d11.py
+++ d11.py
@@ -48,36 +48,38 @@ def largest_total_power_3x3(serial):
 def identifiers():
     for y in range(size):
         for x in range(size):
-            for sq in range(1, min(size - y, size - x)):
-                yield(x, y, sq)
-                
+            for s in range(1, min(size - y, size - x)):
+                yield(x + 1, y + 1, s)
+
+def summed_area_table(grid):
+    table = []
+    for y in range(size):
+        for x in range(size):
+            sum = grid[index(x,y)]
+            if x > 0:
+                sum += table[index(x-1,y)]
+            if y > 0:
+                sum += table[index(x, y-1)]
+            if x > 0 and y > 0:
+                sum -= table[index(x-1, y-1)]
+            table.append(sum)
+    return table
+
+def lookup_area(sat, id):
+    l = id[0]-2
+    r = l+id[2]
+    t = id[1]-2
+    b = t+id[2]
+    sa = sat[index(l, t)] if l >= 0 and t >= 0 else 0
+    sb = sat[index(r, t)] if t >= 0 else 0
+    sc = sat[index(l, b)] if l >= 0 else 0
+    sd = sat[index(r, b)]
+    return sd + sa - sb - sc
+
 def largest_total_power(serial):
     grid = calculate_power_grid(serial)
-    totals = [0 for _ in range(len(grid))]
-    identifier = None
-    largest_power =  None
-    for sq in range(size):
-        limit = size - sq
-
-        # Increment totals.
-        for y in range(0, limit):
-            for x in range(0, limit):
-                power = 0
-                for i in range(0, sq+1):
-                    power += grid[index(x+i, y+sq)]
-                for j in range(0, sq):
-                    power += grid[index(x+sq, y+j)]
-                totals[index(x,y)] += power
-
-        # Find identifier with largest total power.
-        for y in range(0, limit):
-            for x in range(0, limit):
-                power = totals[index(x, y)]
-                if not largest_power or power > largest_power:
-                    largest_power = power
-                    identifer = (x+1,y+1,sq+1)
-
-    return identifer
+    sat = summed_area_table(grid)
+    return max(identifiers(), key=lambda x: lookup_area(sat, x))
 
 if __name__ == "__main__":
     solve(11, parse, largest_total_power_3x3, largest_total_power)
