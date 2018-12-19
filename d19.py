58d69eb236f1d0af64983259d8b29ffb58e13ce6
Alex Gulliver <randdalf@gmail.com>
2018-12-19 08:59:25 +0000

    Day 19, Puzzle 2

--- d19.py
+++ d19.py
@@ -2,6 +2,8 @@
 
 """Advent of Code 2018, Day 19"""
 
+import math
+
 from aoc18 import solve
 from cpu import ops
 
@@ -20,17 +22,31 @@ def parse(data):
         instrs.append((ops_by_name[parts[0]], tuple(map(int, parts[1:]))))
     return Program(ipr, instrs)
 
-def execute(program):
+def execute(program, r, max_cycles):
     ip = 0
     ipr = program.ipr
     instrs = program.instrs
-    r = [0, 0, 0, 0, 0, 0]
-    while 0 <= ip < len(instrs):
+    cycle = 0
+    while 0 <= ip < len(instrs) and cycle < max_cycles:
         r[ipr] = ip
         instr = instrs[ip]
         instr[0](r, *instr[1])
         ip = r[ipr] + 1
+        cycle += 1
     return r[0]
 
+def sum_factors(program, r0):
+    r = [r0,0,0,0,0,0]
+    execute(program, r, 20)
+    n = r[5]
+    sqrt_n = math.sqrt(n)
+    i = 1
+    sum = 0
+    while i <= sqrt_n:
+        if n % i == 0:
+            sum += i + n / i
+        i += 1
+    return int(sum)
+
 if __name__ == "__main__":
-    solve(19, parse, execute)
+    solve(19, parse, lambda p: sum_factors(p, 0), lambda p: sum_factors(p, 1))
