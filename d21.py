b8f230a37fe9fe48843b45e518c62ab6d4d045c9
Alex Gulliver <randdalf@gmail.com>
2018-12-21 17:07:46 +0000

    Day 21, Puzzle 2

--- d21.py
+++ d21.py
@@ -13,8 +13,27 @@ def run_until_halt_opportunity(program):
     r = [0,0,0,0,0,0]
     while True:
         ip = execute(program, r, 1, ip)
-        if r[2] == 29:
-            return r[4]
+        if ip == 28:
+            instr = program.instrs[ip]
+            return r[instr[1][0]]
+
+def run_until_seen(program):
+    r0 = 0
+    r4 = 0
+    seen = set()
+    prev = None
+    while True:
+        r1 = r4 | 65536
+        r4 = 16031208
+        while True:
+            r4 = (((r4 + (r1 & 255)) & 16777215) * 65899) & 16777215
+            if 256 > r1:
+                break
+            r1 = r1 // 256
+        if r4 in seen:
+            return prev
+        seen.add(r4)
+        prev = r4
 
 if __name__ == "__main__":
-    solve(21, parse, run_until_halt_opportunity)
+    solve(21, parse, run_until_halt_opportunity, run_until_seen)
