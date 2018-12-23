5ff782bb154171979d25c96b4a2346d175f03da2
Alex Gulliver <randdalf@gmail.com>
2018-12-23 15:03:16 +0000

    Day 23, Puzzle 2

--- d23-test.py
+++ d23-test.py
@@ -7,6 +7,7 @@ import unittest
 
 from d23 import parse
 from d23 import in_range_of_largest_radius
+from d23 import in_range_of_most_nanobots
 
 example1="""pos=<0,0,0>, r=4
 pos=<1,0,0>, r=1
@@ -18,9 +19,20 @@ pos=<1,1,1>, r=1
 pos=<1,1,2>, r=1
 pos=<1,3,1>, r=1"""
 
+example2="""pos=<10,12,12>, r=2
+pos=<12,14,12>, r=2
+pos=<16,12,12>, r=4
+pos=<14,14,14>, r=6
+pos=<50,50,50>, r=200
+pos=<10,10,10>, r=5"""
+
 class InRangeOfLargestRadiusTests(unittest.TestCase):
     def test_example1(slf):
         slf.assertEqual(in_range_of_largest_radius(parse(example1)), 7)
+
+class InRangeOfMostNanobots(unittest.TestCase):
+    def test_example2(slf):
+        slf.assertEqual(in_range_of_most_nanobots(parse(example2)), 36)
 
 if __name__ == "__main__":
     unittest.main()
--- d23.py
+++ d23.py
@@ -3,6 +3,10 @@
 """Advent of Code 2018, Day 23"""
 
 import re
+from math import log
+from math import ceil
+from random import randint
+from random import uniform
 
 from aoc18 import solve
 
@@ -13,10 +17,60 @@ def parse(data):
 def dist(a, b):
     return abs(a[0] - b[0]) + abs(a[1] - b[1]) + abs(a[2] - b[2])
 
+def in_range(pos, bot):
+    return dist(pos, bot) <= bot[3]
+
 def in_range_of_largest_radius(bots):
     strongest = max(bots, key=lambda b: b[3])
     radius = strongest[3]
-    return sum(int(dist(bot, strongest) <= radius) for bot in bots)
+    return sum(int(in_range(bot, strongest)) for bot in bots)
+
+def score(pos, bots):
+    # 1: Maximise number of in-range bots.
+    # 2: Minimise distance from origin.
+    return (sum(int(in_range(pos, bot)) for bot in bots), -dist(pos, (0,0,0)))
+
+def neighbors(pos, offsets):
+    x0 = pos[0]
+    y0 = pos[1]
+    z0 = pos[2]
+    for offset in offsets:
+        for sign in [-1, +1]:
+            s = sign * offset
+            x1 = x0 + s
+            y1 = y0 + s
+            z1 = z0 + s
+            yield(x0, y0, z1)
+            yield(x0, y1, z0)
+            yield(x0, y1, z1)
+            yield(x1, y0, z0)
+            yield(x1, y0, z1)
+            yield(x1, y1, z0)
+            yield(x1, y1, z1)
+
+def in_range_of_most_nanobots(bots):
+    # Calculate size of world to limit number of neighbors.
+    gmin = [min(z) for z in zip(*[tuple(x - b[3] for x in b[:3]) for b in bots])]
+    gmax = [max(z) for z in zip(*[tuple(x + b[3] for x in b[:3]) for b in bots])]
+    size = max([x-n for n,x in zip(gmin, gmax)])
+    max_k = ceil(log(size / 2, 2))
+    offsets = [2**k for k in range(max_k+1)]
+
+    # Hill climb the problem space.
+    curr = (0,0,0)
+    curr_score = score(curr, bots)
+    improved = True
+
+    while improved:
+        improved = False
+        for neighbor in neighbors(curr, offsets):
+            n_score = score(neighbor, bots)
+            if n_score > curr_score:
+                curr = neighbor
+                curr_score = n_score
+                improved = True
+
+    return -curr_score[1]
 
 if __name__ == "__main__":
-    solve(23, parse, in_range_of_largest_radius)
+    solve(23, parse, in_range_of_largest_radius, in_range_of_most_nanobots)
